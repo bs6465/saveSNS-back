062ab6bc183647a18898289b4d9531d379c082be
bs6465 <qaplazlm41@gmail.com>
2025-12-12 22:09:29 +0900

    Trigger GitHub Action

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config(); // 환경변수는 secrets와 configMap으로 대체됨
 
 const express = require('express');
 // const cors = require('cors'); // cors 라이브러리 불러오기
@@ -6,7 +6,7 @@ const http = require('http');
 // const { initSocketIO } = require('./websocket'); // 웹소켓 핸들러 가져오기
 const app = express();
 const port = 3000;
-  
+
 // 1. 전역 미들웨어 설정
 // app.use(cors());
 app.use(express.json()); // JSON 파싱
