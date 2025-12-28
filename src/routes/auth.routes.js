28394a2ca99793a445df46b1cef7243598bc7bb8
bs6465 <qaplazlm41@gmail.com>
2025-12-28 21:12:36 +0900

    Trigger GitHub Action

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,18 +5,16 @@ const router = express.Router(); // express의 라우터 기능을 사용
 로그인, 회원가입 routes
 */
 
-// 1. 실제 로직이 담긴 '컨트롤러'를 임포트
-// const authController = require('../controllers/auth.controller');
+// 실제 로직이 담긴 '컨트롤러'를 임포트
 import * as authController from '../controllers/auth.controller.js';
-// const { verifyToken, checkAuth } = require('../middleware/authMiddleware'); // 미들웨어 경로
+const { verifyToken, checkAuth } = require('../middleware/authMiddleware'); // 미들웨어 경로
 
-// 2. 경로와 컨트롤러 함수를 연결
+// 경로와 컨트롤러 함수를 연결
 // GET / 모든 유저 가져오기
-// router.get('/', authController.getUsers);
-router.get('/', authController.getUsers);
-router.get('/me', authController.getMe);
+router.get('/', verifyToken, authController.getUsers);
+router.get('/me', [verifyToken, checkAuth], authController.getMe);
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.post('/logout', authController.logout);
+router.post('/logout', [verifyToken], authController.logout);
 
 export default router; // ES6 모듈 방식
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -6,13 +6,11 @@ const router = express.Router(); // express의 라우터 기능을 사용
 */
 
 import * as authController from '../controllers/post.controller.js';
-// const { verifyToken, checkAuth } = require('../middleware/authMiddleware'); // 미들웨어 경로
+const { verifyToken, checkAuth } = require('../middleware/authMiddleware'); // 미들웨어 경로
 
-// 2. 경로와 컨트롤러 함수를 연결
-// GET / 모든 글 가져오기
-// router.get('/', authController.getUsers);
-router.post('/', authController.createPost);
-router.get('/', authController.getPosts);
+// 경로와 컨트롤러 함수를 연결
+router.post('/', [verifyToken, checkAuth], authController.createPost);
+router.get('/', [verifyToken, checkAuth], authController.getPosts);
 router.get('/all', authController.getAllPosts);
 
 export default router;
