33159516f6800d198ab5dcc544328672e8e5a5fd
bs6465 <qaplazlm41@gmail.com>
2025-12-28 21:35:24 +0900

    Trigger GitHub Action

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,20 +1,18 @@
 import express from 'express';
 const router = express.Router(); // express의 라우터 기능을 사용
+import * as authController from '../controllers/auth.controller.js';
+import { verifyToken, checkAuth } from '../middleware/authMiddleware.js';
 
 /*
 로그인, 회원가입 routes
 */
 
-// 실제 로직이 담긴 '컨트롤러'를 임포트
-import * as authController from '../controllers/auth.controller.js';
-import { verifyToken, checkAuth } from '../middleware/authMiddleware.js';
-
 // 경로와 컨트롤러 함수를 연결
-// GET / 모든 유저 가져오기
-router.get('/', verifyToken, authController.getUsers);
-router.get('/me', [verifyToken, checkAuth], authController.getMe);
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.post('/logout', [verifyToken], authController.logout);
+
+router.get('/', verifyToken, authController.getUsers);                // GET / 모든 유저 가져오기
+router.get('/me', [verifyToken, checkAuth], authController.getMe);    // GET / 내 정보 가져오기
+router.post('/register', authController.register);                    // POST / 회원가입
+router.post('/login', authController.login);                          // POST / 로그인
+router.post('/logout', [verifyToken], authController.logout);         // POST / 로그아웃
 
 export default router; // ES6 모듈 방식
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,16 +1,15 @@
 import express from 'express';
 const router = express.Router(); // express의 라우터 기능을 사용
+import * as authController from '../controllers/post.controller.js';
+import { verifyToken, checkAuth } from '../middleware/authMiddleware.js';
 
 /*
 게시글 routes
 */
 
-import * as authController from '../controllers/post.controller.js';
-import { verifyToken, checkAuth } from ('../middleware/authMiddleware.js'); // 미들웨어 경로
-
 // 경로와 컨트롤러 함수를 연결
-router.post('/', [verifyToken, checkAuth], authController.createPost);
-router.get('/', [verifyToken, checkAuth], authController.getPosts);
-router.get('/all', authController.getAllPosts);
+router.post('/', [verifyToken, checkAuth], authController.createPost); // POST / 글 작성
+router.get('/', [verifyToken, checkAuth], authController.getPosts); // GET / 글 목록 조회
+router.get('/all', authController.getAllPosts); // GET / 글 전체 조회
 
 export default router;
