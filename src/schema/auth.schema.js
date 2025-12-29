33ebaf52f611dee8b172a815fb21c1cb5de6819f
bs6465 <qaplazlm41@gmail.com>
2025-12-29 20:49:29 +0900

    Trigger GitHub Action

--- src/schema/auth.schema.js
+++ src/schema/auth.schema.js
@@ -7,7 +7,7 @@ import { z } from 'zod';
 // 회원가입 스키마, validateBody 미들웨어에서 사용
 export const registerSchema = z.object({
   username: z.string().min(3, 'username은 최소 3자 이상이어야 합니다.'),
-  password: z.string().min(6, 'password는 최소 6자 이상이어야 합니다.'),
+  password: z.string().min(3, 'password는 최소 3자 이상이어야 합니다.'),
   longitude: z.number().optional(),
   latitude: z.number().optional(),
 });
@@ -15,7 +15,7 @@ export const registerSchema = z.object({
 // 로그인 스키마, validateBody 미들웨어에서 사용
 export const loginSchema = z.object({
   username: z.string().min(3, 'username은 최소 3자 이상이어야 합니다.'),
-  password: z.string().min(6, 'password는 최소 6자 이상이어야 합니다.'),
+  password: z.string().min(3, 'password는 최소 3자 이상이어야 합니다.'),
 });
 
 // 로그인 확인 스키마, validateToken 미들웨어에서 사용
