fdb8eb66249bc375001e80b1924b2636d6417dd4
bs6465 <qaplazlm41@gmail.com>
2026-01-30 16:15:04 +0900

    Trigger GitHub Action

--- src/config/s3.config.js
+++ src/config/s3.config.js
@@ -9,8 +9,8 @@ import { randomBytes } from 'crypto';
 const s3Client = new S3Client({
   region: process.env.AWS_REGION || 'ap-northeast-2',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
   },
 });
 
